505f0cec7de35aefb8f7adb2da6e8d13e1c0e54e
Nic Chan <channicoleelise@gmail.com>
2020-11-30 17:20:22 +0800

    Initial commit with starter kit and functional build pipeline

---
+++ .eleventy.js
@@ -0,0 +1,140 @@
+const syntaxHighlightPlugin = require('@11ty/eleventy-plugin-syntaxhighlight')
+const htmlMinTransform = require('./utils/transforms/htmlmin.js')
+const contentParser = require('./utils/transforms/contentParser.js')
+const htmlDate = require('./utils/filters/htmlDate.js')
+const rssPlugin = require('@11ty/eleventy-plugin-rss')
+const pwaPlugin = require('eleventy-plugin-pwa')
+const date = require('./utils/filters/date.js')
+const fs = require('fs')
+
+/**
+ * Import site configuration
+ */
+const siteConfig = require('./src/_data/config.json')
+
+module.exports = function (eleventyConfig) {
+  /**
+   * Add custom watch targets
+   *
+   * @link https://www.11ty.dev/docs/config/#add-your-own-watch-targets
+   */
+  eleventyConfig.addWatchTarget('./bundle/')
+
+  /**
+   * Passthrough file copy
+   *
+   * @link https://www.11ty.io/docs/copy/
+   */
+  eleventyConfig.addPassthroughCopy({
+    './static': '.'
+  })
+  eleventyConfig.addPassthroughCopy(`./src/assets/css/${siteConfig.syntaxTheme}`)
+  eleventyConfig.addPassthroughCopy({
+    bundle: 'assets'
+  })
+
+  /**
+   * Add filters
+   *
+   * @link https://www.11ty.io/docs/filters/
+   */
+  // human friendly date format
+  eleventyConfig.addFilter('dateFilter', date)
+  // robot friendly date format for crawlers
+  eleventyConfig.addFilter('htmlDate', htmlDate)
+
+  /**
+   * Add Transforms
+   *
+   * @link https://www.11ty.io/docs/config/#transforms
+   */
+  if (process.env.ELEVENTY_ENV === 'production') {
+    // Minify HTML when building for production
+    eleventyConfig.addTransform('htmlmin', htmlMinTransform)
+  }
+  // Parse the page HTML content and perform some manipulation
+  eleventyConfig.addTransform('contentParser', contentParser)
+
+  /**
+   * Add Plugins
+   * @link https://github.com/11ty/eleventy-plugin-rss
+   * @link https://github.com/11ty/eleventy-plugin-syntaxhighlight
+   * @link https://github.com/okitavera/eleventy-plugin-pwa
+   */
+  eleventyConfig.addPlugin(rssPlugin)
+  eleventyConfig.addPlugin(syntaxHighlightPlugin)
+  eleventyConfig.addPlugin(pwaPlugin)
+
+  /**
+   * Create custom data collections
+   * for blog and feed
+   * Code from https://github.com/hankchizljaw/hylia
+   */
+  // Blog posts collection
+  const now = new Date()
+  const livePosts = post => post.date <= now && !post.data.draft
+  eleventyConfig.addCollection('posts', collection => {
+    return [
+      ...collection
+      .getFilteredByGlob(
+        `./${siteConfig.paths.src}/${siteConfig.paths.blogdir}/**/*`
+      )
+      .filter(livePosts),
+    ]
+  })
+
+  /**
+   * Override BrowserSync Server options
+   *
+   * @link https://www.11ty.dev/docs/config/#override-browsersync-server-options
+   */
+  eleventyConfig.setBrowserSyncConfig({
+    notify: false,
+    open: true,
+    snippetOptions: {
+      rule: {
+        match: /<\/head>/i,
+        fn: function (snippet, match) {
+          return snippet + match
+        },
+      },
+    },
+    // Set local server 404 fallback
+    callbacks: {
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync('dist/404.html')
+
+        browserSync.addMiddleware('*', (req, res) => {
+          // Provides the 404 content without redirect.
+          res.writeHead(404, {
+            'Content-Type': 'text/html'
+          });
+          res.write(content_404)
+          res.end()
+        })
+      },
+    },
+  })
+
+  /*
+   * Disable use gitignore for avoiding ignoring of /bundle folder during watch
+   * https://www.11ty.dev/docs/ignores/#opt-out-of-using-.gitignore
+   */
+  eleventyConfig.setUseGitIgnore(false);
+
+  /**
+   * Eleventy configuration object
+   */
+  return {
+    dir: {
+      input: siteConfig.paths.src,
+      includes: siteConfig.paths.includes,
+      layouts: `${siteConfig.paths.includes}/layouts`,
+      output: siteConfig.paths.output,
+    },
+    passthroughFileCopy: true,
+    templateFormats: ['njk', 'md'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+  }
+}
