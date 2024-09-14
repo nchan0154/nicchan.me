031cd4ad83f9a3145aef5287855c8321f61261d1
Nic Chan <channicoleelise@gmail.com>
2024-09-14 11:08:15 +0800

    Add tinlytics to site

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
-const commonjs = require("rollup-plugin-commonjs");
-const nodeResolve = require("rollup-plugin-node-resolve");
-import { terser } from "rollup-plugin-terser";
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 
 export default {
-  input: "src/assets/js/app.js",
-  output: {
-    file: "assets/js/app.js",
-    format: "cjs",
-  },
-  plugins: [nodeResolve({ browser: true }), commonjs(), terser()],
+	input: "src/assets/js/app.js",
+	output: {
+		file: "assets/js/app.js",
+		format: "cjs",
+	},
+	plugins: [nodeResolve({ browser: true }), commonjs(), terser()],
 };
